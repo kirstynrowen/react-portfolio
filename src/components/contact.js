bc8e0fa0dea0407ea52c4cb111fc2c9c8f1dd601
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-26 18:43:31 -0700

    build contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,11 +7,66 @@ function Contact() {
         email: '',
         message: '',
     });
+    const [errorMessage, setErrorMessage] = useState('');
 
     const { name, email, message } = formState;
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!validateAndFormatEmail(email)) {
+            setErrorMessage('Please enter a valid email address.');
+            return;
+        } else {
+            setErrorMessage('');
+        }
+        console.log(formState);
+        // setFormState({ name: '', email: '', message: '' });
+    };
 
+    const handleChange = (e) => {
+        if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required`);
+        } else {
+            setErrorMessage('');
+        }
+
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value });
+            console.log(formState);
+        }
+    };
+
+    return(
+        <div>
+            <h2>Contact Me</h2>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Email:</label>
+                    <input 
+                        type="email"
+                        name="email"
+                        defaultValue={email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea
+                        name="message"
+                        rows="5"
+                        defaultValue={message}
+                        onChange={handleChange}
+                    />
+                </div>
+                {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+                )}
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
+
+export default Contact;
